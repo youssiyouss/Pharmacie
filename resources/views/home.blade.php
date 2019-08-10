47e1798458930f3301ac423ee1be1af9b00ed088
youssera <youssiyoussera.tlm@gmail.com>
2019-08-10 16:19:11 +0200

    ajoutTemplate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-=======
 @extends('layouts.admin')
 
 @section('content')
@@ -331,5 +307,4 @@
                     </div>
                 </div>
             </div>
->>>>>>> fc7ee82837439983b5bef8ccc956f639437ba185
 @endsection
