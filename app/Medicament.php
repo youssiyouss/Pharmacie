4d819122f90264f554b9f45de0aaee305df29e1a
youssera <youssiyoussera.tlm@gmail.com>
2019-08-29 00:20:04 +0200

    la3ya

--- app/Medicament.php
+++ app/Medicament.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App;
+
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Medicament extends Model
 {
+    //
      use SoftDeletes;
+
+
      protected $dates = ['deleted_at'];
 }
